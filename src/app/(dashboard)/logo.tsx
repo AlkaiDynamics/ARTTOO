596c643d4f6b6edab97715a3e137197b41610c15
AlkaiDynamics <163759418+AlkaiDynamics@users.noreply.github.com>
2025-06-03 15:37:32 -0700

    Apply ARTTOO branding: colors, logo, and name

--- src/app/(dashboard)/logo.tsx
+++ src/app/(dashboard)/logo.tsx
@@ -14,9 +14,9 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-2 hover:opacity-75 transition h-[68px] px-4">
         <div className="size-8 relative">
-          <Image src="/logo.svg" alt="The Canvas" fill />
+          <Image src="/arttoo_logo.png" alt="ARTTOO" fill />
         </div>
-        <h1 className={cn(font.className, "text-xl font-bold")}>The Canvas</h1>
+        <h1 className={cn(font.className, "text-xl font-bold")}>ARTTOO</h1>
       </div>
     </Link>
   );
